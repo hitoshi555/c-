16f4b05295cab091244ce6edd3e024d3e17027f3
hitoshi555 <hitoshi1845@gmail.com>
2024-05-01 01:15:13 +0900

    add EX11 and EX12

---
+++ APG4b/EX11.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N, A;
+    cin >> N >> A;
+
+    // ここにプログラムを追記
+    for (int i = 0; i < N; i++)
+    {
+        int B;
+        string op;
+        cin >> op >> B;
+
+        if (op == "+")
+        {
+            A = A + B;
+        }
+        else if (op == "-")
+        {
+            A = A - B;
+        }
+        else if (op == "*")
+        {
+            A = A * B;
+        }
+        else if (op == "/" && B > 0)
+        {
+            A = A / B;
+        }
+        else
+        {
+            cout << "error" << endl;
+            break;
+        }
+
+        cout << i + 1 << ":" << A << endl;
+    }
+}
---
+++ APG4b/EX12.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    int answer = 1;
+
+    // ここにプログラムを追記
+    for (int i = 0; i < S.size(); i++)
+    {
+        if (S.at(i) == '+')
+        {
+            answer++;
+        }
+        if (S.at(i) == '-')
+        {
+            answer--;
+        }
+    }
+
+    cout << answer << endl;
+}
