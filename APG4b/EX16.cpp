3043e1231a02d25c4cd2b1005fc68523aab051ae
hitoshi555 <hitoshi1845@gmail.com>
2024-05-02 02:13:29 +0900

    add 16, 17 and 18

---
+++ APG4b/EX16.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> data(5);
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> data.at(i);
+    }
+
+    // dataの中で隣り合う等しい要素が存在するなら"YES"を出力し、そうでなければ"NO"を出力する
+    // ここにプログラムを追記
+
+    // dataの中で隣り合う等しい要素が存在するなら"YES"を出力し、そうでなければ"NO"を出力する
+    bool ans = false; // 始めはfalseにしておき、条件を満たすときにtrueになるようにする
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (data.at(i) == data.at(i + 1))
+        {
+            /* ここを変える */
+            ans = true;
+        }
+    }
+
+    if (ans)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
